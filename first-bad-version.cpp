ee4caf97898ddb4557cf2c6f20ac2fa8c7ad73ea
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-06 20:11:39 -0800

    fixed edge cases

--- first-bad-version.cpp
+++ first-bad-version.cpp
@@ -6,6 +6,7 @@ bool isBadVersion(int version);  // API which returns whether version is bad.
 int firstBadVersion(int n);
 
 int main(int argc, char const *argv[]) {
+  cout << "first bad version: " << firstBadVersion(3) << endl;
   return 0;
 }
 
@@ -16,19 +17,26 @@ int firstBadVersion(int n) {
   int end = n;
   int mid = (end - start) / 2;
 
+  // if there are no (0) versions
   if (n == 0)
     return 0;
+
+  // if there is only 1 version
   if (n == 1)
     return isBadVersion(n);
+
+  // if the very first version is bad
+  if (isBadVersion(1) == true)
+    return 1;
 
   while ((end - start) >= 0) {
     mid = (end - start) / 2;
     if ((isBadVersion(start) == false) && (isBadVersion(start + 1) == true)) {
-      return start;
+      return (start + 1);
     }
 
     if ((isBadVersion(end) == true) && (isBadVersion(end - 1) == false)) {
-      return (end - 1);
+      return end;
     }
     if (isBadVersion(mid) == true) {
       // mid version is bad that means our first bad version is
@@ -42,14 +50,15 @@ int firstBadVersion(int n) {
       end--;
     }
   }
+  return -1;
 }
 
 // API which returns whether version is bad.
 // returns true if the version is bad
 // returns false if the version is good
 bool isBadVersion(int version) {
-  if (version < 20)
-    return false;
-  else
+  if (version >= 1)
     return true;
+  else
+    return false;
 }
